7d3c9878e5d1623ef0b113e98827f7140813dc45
Victor Jifcu <victorjifcu@victors-imac.local>
2017-06-04 16:38:59 -0400

    Initial Commit

---
+++ Spellbook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spellbook
+//
+//  Created by Victor Jifcu on 2017-06-04.
+//  Copyright © 2017 Victor Jifcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
